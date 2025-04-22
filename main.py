7f34736314636d3cd7e50cf31953a1489bcbc24f
juan-i94 <juan.ignacio.rabellino@hotmail.com>
2025-04-22 18:47:27 +0000

    get file, read file, print num_words

---
+++ main.py
@@ -0,0 +1,13 @@
+def get_book_text(path):
+    with open(path) as f:
+        file_contents = f.read()
+    return file_contents
+
+
+def main():
+    contents = get_book_text("books/frankenstein.txt")
+    num_words = len(contents.split())
+    print(f"{num_words} words found in the document")
+
+
+main()
