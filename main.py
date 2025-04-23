81e2a8399bdeb250a943d7fe995e73d7cf7d5ec4
juan-i94 <juan.ignacio.rabellino@hotmail.com>
2025-04-23 07:15:27 +0000

    crunching numbers done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from stats import get_num_words
-
+from stats import count_chars
 def get_book_text(path):
     with open(path) as f:
        text = f.read()
@@ -9,7 +9,8 @@ def get_book_text(path):
 def main():
     text = get_book_text("books/frankenstein.txt")
     num_words = get_num_words(text)
+    chars = count_chars(text.lower()) 
     print(f"{num_words} words found in the document")
-
+    print(chars)
 
 main()
--- stats.py
+++ stats.py
@@ -3,4 +3,13 @@ def get_num_words(text):
     return num_words
 
 
+def count_chars(text):
+    char_count = {}
+    for char in text:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
 
+# end of file
